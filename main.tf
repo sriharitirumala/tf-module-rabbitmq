f8945f7acf47500f95cff6e743554621ee8db731
sriharitirumala <sritirumala30@gmail.com>
2023-03-29 09:29:36 +0530

    adding vpc subnets, routes, route table associations

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Request a spot instance at "Lower-Costs"
 resource "aws_spot_instance_request" "rabbitmq" {
-  ami                  = data.tf
+  ami                  = data.aws_ami.ami.id
   instance_type        = var.instance_type
   subnet_id            = var.subnet_ids [0]
   wait_for_fulfillment = true
