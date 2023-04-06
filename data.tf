174d813fc8df236f35761c8251dabb98c142b572
sriharitirumala <sritirumala30@gmail.com>
2023-04-06 20:08:16 +0530

    adding vpc subnets, routes, route table associations

--- data.tf
+++ data.tf
@@ -1,5 +1,12 @@
-data "aws_ami" "ami"  {
+data "aws_ami" "ami" {
   most_recent = true
-  name_regex = "devops-practice-with-ansible"
-  owners = ["self"]
+  name_regex  = "devops-practice-with-ansible"
+  owners      = ["self"]
 }
+
+data "aws_route53_zone" "domain" {
+  name = var.dns_domain
+}
+
+data "aws_caller_identity" "account" {}
+
